402b89f1307ae1300e4d0b7ea54e65410e5f2538
Dipto <tdipto@yahoo.com>
2024-04-10 16:11:59 +0600

    route authenticate

--- client/src/routers/AppRouter.jsx
+++ client/src/routers/AppRouter.jsx
@@ -1,24 +1,70 @@
+import { useQuery } from "@apollo/client";
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
+
 import NavbarComponent from "../components/Navbar/Navbar";
+import NotFound from "../components/NotFound/NotFound";
 import ProductSellForm from "../components/Products/ProductSellForm/ProductSellForm";
+import { GET_USER_QUERY } from "../graphql/queries/userQueries/userQueries";
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/LoginPage";
 import ProfilePage from "../pages/ProfilePage";
 import RegistrationPage from "../pages/RegistrationPage";
 import SingleProductPage from "../pages/SingleProductPage";
 const AppRouter = () => {
+  const { loading, error, data } = useQuery(GET_USER_QUERY);
+
+  if (loading) return <p>Loading...</p>;
+  // if (error) {
+  //   console.error("Error fetching user data:", error);
+  //   return <p>Error fetching user data. Please try again later.</p>;
+  // }
+
+  const isAuthenticated = data?.getCurrentUser !== undefined;
   return (
     <>
       <Router>
         <NavbarComponent />
+        {/* {console.log(data.getCurrentUser)} */}
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/registration" element={<RegistrationPage />} />
-          <Route path="/product/:id" element={<SingleProductPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/product" element={<ProductSellForm />} />
+          <Route
+            path="/profile"
+            element={
+              isAuthenticated ? <ProfilePage /> : <Navigate to="/login" />
+            }
+          />
+          <Route
+            path="/product"
+            element={
+              isAuthenticated ? <ProductSellForm /> : <Navigate to="/login" />
+            }
+          />
+          {/* Routes accessible only when not logged in */}
+          <Route
+            path="/login"
+            element={!isAuthenticated ? <LoginPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/registration"
+            element={
+              !isAuthenticated ? <RegistrationPage /> : <Navigate to="/" />
+            }
+          />
+          {/* Public routes accessible to all */}
+          <Route
+            path="/product/:id"
+            element={
+              isAuthenticated ? <SingleProductPage /> : <Navigate to="/login" />
+            }
+          />
+          <Route path="*" element={<NotFound />} />{" "}
+          {/* Fallback for unknown routes */}
         </Routes>
       </Router>
     </>
